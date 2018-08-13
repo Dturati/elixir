0acf7c96cc929719366f466dab5f76247c1f0e11
DavidTurati <davidturati@gmail.com>
2018-08-13 00:07:23 -0400

    enum

--- enum/enum1.ex
+++ enum/enum1.ex
@@ -1,5 +1,19 @@
 list = ["David","Dos","Santos","Turati"]
 
-res = Enum.all?(list, fn(s) -> String.length(s) >= 3 end)
+numbers = [1,2,3,4,5,6,7]
 
+res = Enum.all?(list, fn(s) -> String.length(s) > 3  end)
 IO.puts res
+
+res = Enum.any?(list, fn(s) -> String.length(s) > 3 end)
+IO.puts res
+
+res2 = Enum.chunk_every([list],2)
+IO.puts res2
+
+Enum.each(list, fn(s) -> IO.puts(s) end)
+
+resMap = Enum.map(numbers, fn(s) -> s*2 end)
+
+IO.inspect Enum.reduce(resMap,fn(a, b) -> a+b end)
+
